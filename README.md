85411a1c46c119c30dfd82fabce69b491527269e
AshBloo <ashjones1993@outlook.com>
2020-12-03 13:52:23 +0000

    Adjusted readme and tidied main.py

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# SimpleEncrypter
-Goal: Rebuilding my old d&amp;d cypher from the ground up. 
-Description: A simple application that takes a string and either encodes or decodes it.
+# Requirements
 
-Changelog
-23/11/2020 - Updated README and added a changelog. Created diary.md to record project progression and thoughts.
+This is a python project that allows the user to encrypt .txt files and save them. It features a graphical user interface and two encryption methods. This application was made with Python 3.9.0 so for best results ensure you are using this version of python or later. You can do this by opening the command prompt (on windows) and typing "python -V" without quotations. This will then print out your version of python. If this brings up an error, you may need to check that you have python installed (as this will not function without it).
 
-1/12/2020 - Created frequencyAnalysis.py and cypherFunctions.py. Split functions off into each and made main.py contain only what is necessary to run the script, pulling extra functions from the two new files.
+# Setup
 
-2/12/2020 - Program now utilises a graphical user interface using the Tkinter module. Users now have the option of loading .txt files and encrypting/decrypting them, and saving the result. At the moment, only Ceasar has been fully implemented.
+To run this application on your own machine, you can fork and clone this repository, then run main.py. This will launch the graphical user interface for you to either type a message into or you can open an existing .txt file. The Ceasar cypher requires an encryption key in the form of a whole number (an integer). The Vigenere cypher requires a keyword in the form of a sequence of letters (a string).
+
+
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import cypherFunctions, frequencyAnalysis
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
@@ -26,35 +25,83 @@ def save_file():
         output_file.write(text)
     window.title(f"Simple Text Encrypter - {filepath}")
 
+def ceasarCypher(message,key,encrypt) -> str: #Remember to remove message
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    output = ""
+    message = message.upper()
+    for symbol in message:
+        if symbol in alphabet:
+            num = alphabet.find(symbol) ## Retrieves number associated with the alphabet
+            if encrypt == True:
+                num = num + key
+            else:
+                num = num - key
+            ## Wrap-around protection
+            if num >= len(alphabet):
+                num = num - len(alphabet)
+            elif num < 0:
+                num = num + len(alphabet)
+            ## Adding generated letter to translated array
+            output = output + alphabet[num]
+        else:
+            output = output + symbol ## If symbol is not recognised as a letter, it is unaltered
+    return output
+
+def vigenereCypher(message,phrase,encrypt) -> str:
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    i = 0
+    output = ""
+    message = message.upper()
+    phrase =  phrase.upper()
+    for symbol in message:
+        if i == len(phrase):
+            i = i - len(phrase)
+        if symbol in alphabet:
+            num = alphabet.find(symbol)
+            shiftFactor = alphabet.find(phrase[i])
+            if encrypt == True:
+                num = num + shiftFactor
+            else:
+                num = num - shiftFactor
+            ## Wrap-around protection
+            if num >= len(alphabet):
+                num = num - len(alphabet)
+            elif num < 0:
+                num = num + len(alphabet)
+            i = i + 1
+            ## Adding generated letter to translated array
+            output = output + alphabet[num]
+        else:
+            output = output + symbol ## If symbol is not recognised as a letter, it is unaltered
+    return output
+
 def encrypt():
     cypher=mode.get()
     if cypher == "ceasar":
         key=int(ent_key.get())
         message=txt_edit.get("1.0", tk.END)
-        translate=cypherFunctions.ceasarCypher(message,key,encrypt=True)
+        translate=ceasarCypher(message,key,encrypt=True)
         txt_edit.delete("1.0", tk.END)
         txt_edit.insert(tk.END, translate)
     if cypher == "vigenere":
         phrase=str(ent_phrase.get())
         message=txt_edit.get("1.0", tk.END)
-        translate=cypherFunctions.vigenereCypher(message,phrase,encrypt=True)
+        translate=vigenereCypher(message,phrase,encrypt=True)
         txt_edit.delete("1.0", tk.END)
         txt_edit.insert(tk.END, translate)
-    else:
-        print("fail")
 
 def decrypt():
     cypher=mode.get()
     if cypher == "ceasar":
         key=int(ent_key.get())
         message=txt_edit.get("1.0", tk.END)
-        translate=cypherFunctions.ceasarCypher(message,key,encrypt=False)
+        translate=ceasarCypher(message,key,encrypt=False)
         txt_edit.delete("1.0", tk.END)
         txt_edit.insert(tk.END, translate)
     if cypher == "vigenere":
         phrase=str(ent_phrase.get())
         message=txt_edit.get("1.0", tk.END)
-        translate=cypherFunctions.vigenereCypher(message,phrase,encrypt=False)
+        translate=vigenereCypher(message,phrase,encrypt=False)
         txt_edit.delete("1.0", tk.END)
         txt_edit.insert(tk.END, translate)
 
